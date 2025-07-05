4891cf6e129e9e293d3c8e2cc532335a79421135
Sheyon565 <chandradvaipayana56@gmail.com>
2025-07-05 18:37:04 +0800

    Fixing token refresh

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -32,7 +32,7 @@ api.interceptors.response.use(
             }
 
             try {
-                const response = await axios.post('http://localhost:8000/api/token/refresh/', {
+                const response = await axios.post('api/token/refresh/', {
                     refresh: refreshToken,
                 });
 
